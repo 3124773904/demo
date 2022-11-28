e9e96dd468d73e7e146d4406bba5528e5d496338
xw <3124773904@qq.com>
2022-11-28 16:12:59 +0800

    加上猫猫码配置

--- src/main/java/com/robot/RobotApplication.java
+++ src/main/java/com/robot/RobotApplication.java
@@ -1,5 +1,6 @@
 package com.robot;
 
+import love.forte.simboot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author hanse*
  * @date 2022/11/28 9:34
  */
+@EnableSimbot
 @SpringBootApplication
 public class RobotApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RobotApplication.class,args);
     }
 }
